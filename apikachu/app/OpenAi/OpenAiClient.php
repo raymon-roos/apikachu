e997f9f1d52e294bb1aa98b8f6dadbe8c1ce0f5e
gilbertadamowicz <2142150@talnet.nl>
2023-12-12 11:40:23 +0100

    feat: new user prompt

--- apikachu/app/OpenAi/OpenAiClient.php
+++ apikachu/app/OpenAi/OpenAiClient.php
@@ -32,7 +32,7 @@ class OpenAiClient
         $generatedPokemon = $this->openAi->chat()->create([
             'model' => 'gpt-3.5-turbo',
             'messages' => [
-                ['role' => 'system', 'content' => 'Create a valid JSON response of some random pokemon a user would want you to generate'],   
+                ['role' => 'system', 'content' => 'Create a new Pokemon character based on the input of the user with the following unique attributes: - Name - Short description less than 80 characters - The type of Pokemon - The category of Pokemon it is - Number of Hit Points or health - The Pokemon\'s length in inches - The Pokemon\'s weight in pounds - The Pokemon\'s power name and description - The Pokemon\'s attack name with description and Hit Points it would cause in damage - The type of Pokemon it is weak against - The type of Pokemon it is resistant against - The retreat cost of the Pokemon - The Pokemon\'s appearance in less than 600 characters - The Pokemon\'s backstory in less than 600 characters Format the response in the following valid JSON object ${JSON.stringify(POKEMON_ATTRIBUTES)}'],   
                 ['role' => 'user', 'content' => $name],
             ],
             'tools' => [
---
+++ docs/collect/brainstorm_gilbert-adamowicz.md
@@ -0,0 +1,33 @@
+# Pokémon API Project Brainstorm
+
+## Idea 1: Pokemon Generator
+
+### Description
+
+Create an API that generates new Pokemon based of input by users.
+
+### Pros
+
+-   Get to work with LLM aka the future of life.
+-   No limit to features we could add to the generator.
+-   Other projects can build upon our project
+
+### Cons
+
+-   Dependent on external LLM api.
+-   LLM costs money
+
+## Idea 2: Pokemon tinder swiper
+
+### Description
+
+Swipe pokemons to see what pokemons people like based of looks
+
+### Pros
+
+-   Getting data on which pokemon are people there favorite
+
+### Cons
+
+-   A lot of frontend work.
+-   No real vallue.
