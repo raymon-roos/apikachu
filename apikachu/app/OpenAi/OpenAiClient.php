83571dabf910b4905b132044e64b32fee54c92b1
WesselWillemsen <wessel.steven.willemsen@gmail.com>
2023-12-12 13:45:52 +0100

    feat: improved prompt for openai by Gilbert

--- apikachu/app/OpenAi/OpenAiClient.php
+++ apikachu/app/OpenAi/OpenAiClient.php
@@ -32,7 +32,7 @@ class OpenAiClient
         $generatedPokemon = $this->openAi->chat()->create([
             'model' => 'gpt-3.5-turbo-1106',
             'messages' => [
-                ['role' => 'system', 'content' => 'Create a new Pokemon character based on the input of the user with the following unique attributes: - Name - Short description less than 80 characters - The type of Pokemon - The category of Pokemon it is - Number of Hit Points or health - The Pokemon\'s length in inches - The Pokemon\'s weight in pounds - The Pokemon\'s power name and description - The Pokemon\'s attack name with description and Hit Points it would cause in damage - The type of Pokemon it is weak against - The type of Pokemon it is resistant against - The retreat cost of the Pokemon - The Pokemon\'s appearance in less than 600 characters - The Pokemon\'s backstory in less than 600 characters Format the response in the following valid JSON object'],   
+                ['role' => 'system', 'content' => 'Create a new Pokemon character based on the input of the user with the following unique attributes: - Name - Short description less than 80 characters - The type of Pokemon - Number of Hit Points or health - Number of attack damage - Number of defense - Number of speed - Number of special attack - Number of special defense - The Pokémon’s abilities - The Pokémon’s moves - The Pokemons evolutions and the level at which this evolution is reached - The Pokemon\'s appearance in less than 600 characters  Format the response in a valid JSON object'],   
                 ['role' => 'user', 'content' => $name],
             ],
             'tools' => [
