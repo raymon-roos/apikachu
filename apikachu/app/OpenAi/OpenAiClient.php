63416ba138b7864dabd878f72bc2ebe701a8310b
WesselWillemsen <wessel.steven.willemsen@gmail.com>
2023-12-12 12:23:09 +0100

    feat: faster model and updated prompt

--- apikachu/app/OpenAi/OpenAiClient.php
+++ apikachu/app/OpenAi/OpenAiClient.php
@@ -30,9 +30,9 @@ class OpenAiClient
     public function generatePokemon($name)
     {
         $generatedPokemon = $this->openAi->chat()->create([
-            'model' => 'gpt-3.5-turbo',
+            'model' => 'gpt-3.5-turbo-1106',
             'messages' => [
-                ['role' => 'system', 'content' => 'Create a new Pokemon character based on the input of the user with the following unique attributes: - Name - Short description less than 80 characters - The type of Pokemon - The category of Pokemon it is - Number of Hit Points or health - The Pokemon\'s length in inches - The Pokemon\'s weight in pounds - The Pokemon\'s power name and description - The Pokemon\'s attack name with description and Hit Points it would cause in damage - The type of Pokemon it is weak against - The type of Pokemon it is resistant against - The retreat cost of the Pokemon - The Pokemon\'s appearance in less than 600 characters - The Pokemon\'s backstory in less than 600 characters Format the response in the following valid JSON object ${JSON.stringify(POKEMON_ATTRIBUTES)}'],   
+                ['role' => 'system', 'content' => 'Create a new Pokemon character based on the input of the user with the following unique attributes: - Name - Short description less than 80 characters - The type of Pokemon - The category of Pokemon it is - Number of Hit Points or health - The Pokemon\'s length in inches - The Pokemon\'s weight in pounds - The Pokemon\'s power name and description - The Pokemon\'s attack name with description and Hit Points it would cause in damage - The type of Pokemon it is weak against - The type of Pokemon it is resistant against - The retreat cost of the Pokemon - The Pokemon\'s appearance in less than 600 characters - The Pokemon\'s backstory in less than 600 characters Format the response in the following valid JSON object'],   
                 ['role' => 'user', 'content' => $name],
             ],
             'tools' => [
@@ -68,14 +68,18 @@ class OpenAiClient
                                 'type' => 'integer',
                                 'description' => 'pokemon generation id, at the moment 10 because we are in generation 10',
                             ],
+                            'image_appearance' => [
+                                'type' => 'string',
+                                'description' => 'pokemon appearance for ai image prompt',
+                            ],
                         ],
-                            'required' => ['name', 'description', 'hp', 'attack', 'defense', 'generation_id'],
+                            'required' => ['name', 'description', 'hp', 'attack', 'defense', 'generation_id', 'image_appearance'],
                         ],
                     ],
                 ],
             ],
-            'max_tokens' => 255,
-            'temperature' => 0.9,
+            // 'max_tokens' => 255,
+            // 'temperature' => 0.9,
         ]);
 
         return $generatedPokemon;
