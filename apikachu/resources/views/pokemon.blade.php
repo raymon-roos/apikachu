acda2ba3bd93eac3e83fc187a4db39e1fa05e5c6
Shazib Syed <shazib.syed@student.rocva.nl>
2023-12-13 15:18:57 +0100

    frontend_style_added

--- apikachu/resources/views/pokemon.blade.php
+++ apikachu/resources/views/pokemon.blade.php
@@ -1,21 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+
+<body class="bg-amber-400">
 
     @if(isset($pokemon))
-        {{ print_r($pokemon); }}
+        <div class="max-w-md mx-auto mt-10 bg-amber-300 p-10 rounded shadow-lg">
+            <h1>Name: {{ $pokemon['name'] }}</h1>
+            <p>Description: {{$pokemon['description']}}</p>
+            <p>Attack: {{$pokemon['attack']}}</p>
+            <p>Defense: {{$pokemon['defense']}}</p>
+            <p>Generation Id: {{ $pokemon['generation_id']}}</p>
+            <p>Image appearance: {{ $pokemon['image_appearance']}}</p>
+        </div>
+
     @endif
 
-    @if(isset($image))
+    @if (isset($image))
         {{-- Display the image --}}
-        <img src="data:image/png;base64,{{ $image }}" alt="Pokemon Image">
+        <div class="max-w-md mx-auto rounded shadow-lg">
+        <img src="data:image/png;base64,{{ $image }}" class="mt-4" width="500" alt="Pokemon Image">
+        </div>
     @endif
-
+    
 </body>
+
 </html>
--- apikachu/resources/views/welcome.blade.php
+++ apikachu/resources/views/welcome.blade.php
@@ -1,20 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    
-    <form action="/api/generate" method="POST">
-        @csrf
-        <input type="text" name="name" placeholder="name"> Name
-        <input type="radio" name="image" value="1"> image
-        <input type="radio" name="image" value="0"> no image
-        <input type="submit">Submit</button>
-    </form>
+
+<body class="bg-amber-400">
+    <div class="flex flex-col md:flex-row gap-4 max-w-lg mx-auto mt-20 bg-amber-300 p-10 rounded shadow-lg">
+        <form action="/api/generate" method="POST" class="col-span">
+            @csrf
+            <input type="text" name="name" placeholder="Name"
+                class="mb-4 mt-1 p-2 border outline-red-500 rounded-lg w-full">
+            <div class="mb-4 space-x-2">
+                <input type="radio" name="image" value="1"
+                    class="text-indigo-500 focus:ring-indigo-500 h-4 w-4"> Image.
+                <input type="radio" name="image" value="0"
+                    class="text-indigo-500 focus:ring-indigo-500 h-4 w-4"> No image.
+            </div>
+            <button type="submit"
+                class="bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-4 focus:ring-red-400 text-white font-medium rounded-full p-2">Submit</button>
+        </form>
+
+    </div>
 
 </body>
+
 </html>
